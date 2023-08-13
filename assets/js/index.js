ee4a6f7d9615456c7bba6cc1f96e970eb4fc747d
Viktoriia <pecourses>
2023-08-13 16:15:03 +0300

    Add type conversion

--- assets/js/index.js
+++ assets/js/index.js
@@ -165,25 +165,61 @@
 // console.log(greeting + age + ' years old');
 
 // Скорочені операції +=, *=, -=, /=, ...
-let sum = 5;
-sum += 5; // sum = sum + 5;
-console.log(sum);
-sum *= 10; // sum= sum*10;
-console.log(sum);
+// let sum = 5;
+// sum += 5; // sum = sum + 5;
+// console.log(sum);
+// sum *= 10; // sum= sum*10;
+// console.log(sum);
 
-// інкремент постфіксний
-sum++; // sum += 1; // sum = sum + 1;
-console.log(sum);
+// // інкремент постфіксний
+// sum++; // sum += 1; // sum = sum + 1;
+// console.log(sum);
 
-let x = 10;
-const y = x++;
-console.log('y=', y);
-console.log(x);
+// let x = 10;
+// const y = x++;
+// console.log('y=', y);
+// console.log(x);
 
-// інкремент префікс
-let x1 = 10;
-const y1 = ++x1;
-console.log('y1=', y1);
-console.log(x1);
+// // інкремент префікс
+// let x1 = 10;
+// const y1 = ++x1;
+// console.log('y1=', y1);
+// console.log(x1);
 
 // декремент: --x, x--
+
+/////////////////////////////////////
+// Ввести 2 числа і вивести їхню суму
+// const a1 = prompt('Введіть перше число:');
+// // const a2 = prompt('Введіть друге число:');
+// const a2 = 100;
+// const sum = a1 + a1;
+// console.log(sum);
+
+// Приведення типів (явне)
+// result = Number(source)
+// const res1 = Number('10');
+// console.log(res1);
+
+// const res2 = Number(null);
+// console.log(res2);
+
+// const res3 = String(200); // '200'
+// console.log(res3);
+
+// Ввести 2 числа і вивести їхню суму 2
+// const op1 = Number(prompt('Введіть перше число:'));
+// const op2 = +prompt('Введіть друге число:');
+// const sum = op1 + op2;
+// console.log(sum);
+
+// Ввести 3 числа і порахувати їх добуток
+// const a = +prompt('First number');
+// const b = +prompt('Second number');
+// const c = +prompt('Third number');
+// let sum = a * b * c;
+// console.log(sum);
+
+// Приведення типів (неявне) - варто уникати усюди, де можна
+console.log("10 + '10' = ", 10 + '10');
+console.log("null + '10' = ", null + '10');
